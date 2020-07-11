207309c0dce245275eca022608cdd68fa228c8fe
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-07-12 00:46:26 +0900

    Handling HTTP GET Requests in courses/:id

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,6 +2,12 @@ const express = require("express"); // this returns a function
 const app = express(); // call this function to return an object of type 'Express'
 // The 'app' object conventionally denotes the Express application
 
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
 // implement a couple of endpoints that respond to an http get request
 
 // this is how we define a route: specify a path/url,
@@ -13,18 +19,25 @@ app.get("/", (request, response) => {
 app.get("/api/courses", (req, res) => {
   // in a real scenario here we want to get a list of courses from the database and return them
   // res.send([1, 2, 3]); // so here we instead simply return an array of numbers
-  res.send({ test: true }); // so here we instead simply return an array of numbers
+  // res.send({ test: true }); //
+  res.send(courses);
 });
 
 // Route paramter :id
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  // res.send(req.params.id);
   // example: http://localhost:3000/api/posts/2018/1
+
+  //write some logic to look for the course with the given id
+  const course = courses.find((c) => c.id === parseInt(req.params.id)); // will return a boolean value
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+  res.send(course);
 });
 
 app.get("/api/posts/:year/:month", (req, res) => {
-  // res.send(req.params); // how to view all parametrs passed
-  res.send(req.query); // viewing additional optional parameters (http://localhost:5000/api/posts/2018/3?sortBy=name)
+  // res.send(req.params); // how to view all parametrs passed - http://localhost:5000/api/posts/2018/3?sortBy=name
+  res.send(req.query); // viewing additional optional parameters - http://localhost:5000/api/posts/2018/3?sortBy=name
 });
 
 // PORT - set environement variable with the 'process' object
